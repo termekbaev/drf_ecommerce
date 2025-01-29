e47e05b029d89184bb632ddae7a3832952bc4c28
Timur <t-ermekbaev@mail.ru>
2025-01-29 16:27:36 +0300

    Created Category model based on BaseModel

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,29 @@
+from autoslug import AutoSlugField
 from django.db import models
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+class Category(BaseModel):
+    """
+    Represents a product category.
+
+    Attributes:
+        name (str): The category name, unique for each instance.
+        slug (str): The slug generated from the name, used in URLs.
+        image (ImageField): An image representing the category.
+
+    Methods:
+        __str__():
+            Returns the string representation of the category name.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=True)
+    image = models.ImageField(upload_to="category_images/")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = "Categories"
