72719c4461620d64837b19d9acfac781d0c67080
Timur <t-ermekbaev@mail.ru>
2025-01-29 16:17:07 +0300

    Created Seller model based on BaseModel

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.common.models import BaseModel
+
+
+class Seller(BaseModel):
+    # Link to the User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="seller")
+
+    # Business Information
+    business_name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from="business_name", always_update=True, null=True)
+    inn_identification_number = models.CharField(max_length=50)
+    website_url = models.URLField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20)
+    business_description = models.TextField()
+
+    # Address Information
+    business_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    # Bank information
+    bank_name = models.CharField(max_length=255)
+    bank_bic_number = models.CharField(max_length=9)
+    bank_account_number = models.CharField(max_length=50)
+    bank_routing_number = models.CharField(max_length=50)
+
+    # Status fields
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Seller for {self.business_name}"
